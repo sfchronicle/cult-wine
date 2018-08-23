f99b4ade9b0643e06e75682af41c08306609bd06
Lucio Villa <lucio310@gmail.com>
2018-08-23 11:49:24 -0700

    removed document ready for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
 
-	if ($(window).width() < 768) {
-	  $('#header').css('opacity', '1'); 
-	}
-	else {
+if ($(window).width() < 768) {
+  $('#header').css('opacity', '1'); 
+}
+else {
+
+	$(document).ready(function() {
 	  $('#header').delay(5250).queue(function (next) { 
 	   	$(this).css('opacity', '1'); 
 	  	next(); 
 	  });
-	}
-  
-});
+	});
+
+}
+
